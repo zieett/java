e21a57d0df4c8730ae937856f1a30c7bff5a366b
Ziet <viettranvuhoang@gmail.com>
2022-01-15 20:54:39 +0700

    updated

---
+++ src/main/java/rmit/rmitsb/RmitsbApplication.java
@@ -0,0 +1,12 @@
+package rmit.rmitsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmitsbApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RmitsbApplication.class, args);
+	}
+
+}
